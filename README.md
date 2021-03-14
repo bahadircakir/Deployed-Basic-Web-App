291c2ffd69fc92768d7f85d7e4fb995def7150c7
bahadir <cakirbahadir26@gmail.com>
2021-03-14 17:34:03 +0300

    Added some text to help users and people who will fork this project

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is my first gradle project with CI/CD deployed on Heroku.
 [![Build Status](https://travis-ci.com/bahadircakir/containsWebApp.svg?branch=main)](https://travis-ci.com/bahadircakir/containsWebApp)
 
 feature/xyz 
+
+App.java -> searchOnSubArray method is searching an element to whether exists in the subarray.
--- src/main/java/containsWebApp/App.java
+++ src/main/java/containsWebApp/App.java
@@ -80,7 +80,7 @@ public class App {
         get("/compute",
             (rq, rs) -> {
                 Map<String, String> map = new HashMap<String, String>();
-                map.put("result", "not computed yet!");
+                map.put("result", "not computed yet! (Give a list to left box - Give a 2 indexes for subarray then give a value to search to right box)");
                 return new ModelAndView(map, "compute.mustache");
             },
             new MustacheTemplateEngine()
